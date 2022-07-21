77acb5f0f1a4d5a5e950553b6b6f4966bfd0eb5c
Anshul Patel <anshulpatel5757@gmail.com>
2022-07-22 02:21:15 +0530

    Updates

---
+++ precache-manifest.e43d7acff52b5507e5ba20bf8f11b1af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68148508117ac1501bbad3cb2fed76f",
+    "url": "/ContactCards/index.html"
+  },
+  {
+    "revision": "1fd4460b359193168c9c",
+    "url": "/ContactCards/static/js/2.e8ec9a7d.chunk.js"
+  },
+  {
+    "revision": "f895292c595d536b31fb",
+    "url": "/ContactCards/static/js/main.053f6469.chunk.js"
+  },
+  {
+    "revision": "40a598e2bee65ac06d87",
+    "url": "/ContactCards/static/js/runtime-main.42166346.js"
+  }
+]);
